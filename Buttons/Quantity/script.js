5fa899186177dee6b88aab58c47c90986ac84e9b
Yury Notiron <not.iron.yuriy@inbox.ru>
2024-10-17 10:54:48 +0300

    input type='number' (кастомный)
    Changes to be committed:
    	new file:   Buttons/Quantity/index.html
    	new file:   Buttons/Quantity/script.js
    	new file:   Buttons/Quantity/styles.css

---
+++ Buttons/Quantity/script.js
@@ -0,0 +1,15 @@
+  const buttonDecrease = document.getElementById('button-decrease');
+  const buttonIncrease = document.getElementById('button-increase');
+  const input = document.getElementById('quantity-input');
+
+  buttonDecrease.onclick = decreaseQuantity;
+  buttonIncrease.onclick = increaseQuantity;
+
+  function decreaseQuantity() {
+    if (input.value > 1) {
+      input.value = parseInt(input.value) - 1;
+    }
+  }
+  function increaseQuantity() {
+    input.value = parseInt(input.value) + 1;
+  }
