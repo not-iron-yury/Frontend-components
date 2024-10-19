5eee9fb9031c0b2c0556455fe75725fa4743efd3
Yury Notiron <not.iron.yuriy@inbox.ru>
2024-10-19 11:08:25 +0300

    Ссылки на соц сети
    Changes to be committed:
    	new file:   Lists/social link/index.html

--- inputs/Quantity/script.js
+++ inputs/Quantity/script.js
@@ -1,15 +1,15 @@
-  const buttonDecrease = document.getElementById('button-decrease');
-  const buttonIncrease = document.getElementById('button-increase');
-  const input = document.getElementById('quantity-input');
+const buttonDecrease = document.getElementById('button-decrease');
+const buttonIncrease = document.getElementById('button-increase');
+const input = document.getElementById('quantity-input');
 
-  buttonDecrease.onclick = decreaseQuantity;
-  buttonIncrease.onclick = increaseQuantity;
+buttonDecrease.onclick = decreaseQuantity;
+buttonIncrease.onclick = increaseQuantity;
 
-  function decreaseQuantity() {
-    if (input.value > 1) {
-      input.value = parseInt(input.value) - 1;
-    }
+function decreaseQuantity() {
+  if (input.value > 1) {
+    input.value = parseInt(input.value) - 1;
   }
-  function increaseQuantity() {
-    input.value = parseInt(input.value) + 1;
-  }
+}
+function increaseQuantity() {
+  input.value = parseInt(input.value) + 1;
+}
